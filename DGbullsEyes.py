4110deb4cddee5dba0ff5075aad11ba1c97e77df
csPython <cspython@AD25TF04NGF1J-89.local>
2019-03-12 12:14:47 -0400

    Bulls eye is drawn with the correct color

--- DGbullsEyes.py
+++ DGbullsEyes.py
@@ -7,9 +7,14 @@ while True:
     except ValueError:print("Please enter a number")
 
 bullEyeWin = GraphWin("Bulls Eye", windowSize,windowSize);bullEyeWin.setCoords(0,0, windowSize,windowSize)
-r = random.randint(5,20)
 
-for i in range(r):
-    bullsEyeCircle = Circle(Point(windowSize/2,windowSize/2),((windowSize/3)/r) * i)
+def bulleye(r,colorR,colorG,colorB):
+    for i in range(r):
+        bullsEyeCircle = Circle(Point(windowSize/2,windowSize/2),((windowSize/4)/r) * (r-i))
+        
+        if i % 2 == 0:bullsEyeCircle.setFill(color_rgb(colorR,colorG,colorB))
+        else:bullsEyeCircle.setFill(color_rgb(0,0,0))
+        
+        bullsEyeCircle.draw(bullEyeWin)
 
-    bullsEyeCircle.draw(bullEyeWin)
+bulleye(random.randint(5,15),random.randint(0,255),random.randint(0,255),random.randint(0,255))
